6a260f67c8519939b41330f006804574cfe2fa6a
Viktoria Fabrikova <104639463+fvi07@users.noreply.github.com>
2022-05-13 18:42:32 +0300

    v1.2

--- ping-pong.py
+++ ping-pong.py
@@ -1,4 +1,5 @@
 from pygame import *
+font.init()
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y,player_w,player_h, player_speed):
@@ -32,26 +33,50 @@ class Player(GameSprite):
 
 window = display.set_mode((1000,700))
 display.set_caption('Пинг-понг')
-background = transform.scale(image.load('фон.jpg'),(1000,700))
+background = transform.scale(image.load('bg.jpg'),(1000,700))
 player1 = Player('rocket-left.png',5,250,20,150,5)
 player2 = Player('rocket-right.png',975,250,20,150,5)
-
+ball = GameSprite('ball.png',475,325,100,100,10)
 
 game = True
 finish = False
 clock = time.Clock()
 FPS = 60
+speed_x = 10
+speed_y = 10
+font1 = font.SysFont('Arial', 35)
+lose1 = font1.render('PLAYER 1 LOSE',True, (255,0,0))
+lose2 = font1.render('PLAYER 2 LOSE',True, (255,0,0))
 
 while game:
     for e in event.get():
         if e.type == QUIT:
-            game = False
+            game = False 
+    if finish != True:
+        
+        window.blit(background,(0,0))
+        player1.reset()
+        player2.reset()
+        player1.update_left()
+        player2.update_right()
+        ball.reset()
 
-    window.blit(background,(0,0))
-    player1.reset()
-    player2.reset()
-    player1.update_left()
-    player2.update_right()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if ball.rect.y < 0 or ball.rect.y > 600:
+            speed_y *= -1
+
+        if sprite.collide_rect(ball,player1) or sprite.collide_rect(ball,player2):
+            speed_x *= -1
+        
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1,(375,325))
+        if ball.rect.x > 900:
+            finish = True
+            window.blit(lose2,(375,325))
+        
 
     clock.tick(FPS)
     display.update()
