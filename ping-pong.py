9fc9458abd8ed63abf3db853fe477f7a07b83a52
Viktoria Fabrikova <104639463+fvi07@users.noreply.github.com>
2022-05-20 18:22:11 +0300

    v1.3

--- ping-pong.py
+++ ping-pong.py
@@ -2,7 +2,7 @@ from pygame import *
 font.init()
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y,player_w,player_h, player_speed):
+    def __init__(self, player_image, player_x, player_y,player_w,player_h, player_speed,speed_x,speed_y):
         super().__init__()
         self.w = player_w
         self.h = player_h
@@ -11,6 +11,8 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+        self.speed_x = speed_x
+        self.speed_y = speed_y
         
     
     def reset(self):
@@ -30,20 +32,40 @@ class Player(GameSprite):
         if keys_pressed[K_s] and self.rect.y < 545:
             self.rect.y += self.speed 
 
+class Ball(GameSprite):
+    def update(self):
+    
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+
+        if self.rect.y < 0 or self.rect.y > 600:
+            self.speed_y *= -1
+
+        if sprite.collide_rect(self,player1) or sprite.collide_rect(self,player2):
+            self.speed_x *= -1
+
+        if self.rect.x < 0:
+            finish = True
+            window.blit(lose1,(375,325))
+        if self.rect.x > 900:
+            finish = True
+            window.blit(lose2,(375,325))
+        
+
 
 window = display.set_mode((1000,700))
 display.set_caption('Пинг-понг')
-background = transform.scale(image.load('bg.jpg'),(1000,700))
-player1 = Player('rocket-left.png',5,250,20,150,5)
-player2 = Player('rocket-right.png',975,250,20,150,5)
-ball = GameSprite('ball.png',475,325,100,100,10)
+background = transform.scale(image.load('background.jpg'),(1000,700))
+player1 = Player('rocket-left.png',5,250,20,150,5,0,0)
+player2 = Player('rocket-right.png',975,250,20,150,5,0,0)
+ball = Ball('ball.png',475,325,100,100,6,6,6)
 
 game = True
 finish = False
 clock = time.Clock()
 FPS = 60
-speed_x = 10
-speed_y = 10
+speed_x = 6
+speed_y = 6
 font1 = font.SysFont('Arial', 35)
 lose1 = font1.render('PLAYER 1 LOSE',True, (255,0,0))
 lose2 = font1.render('PLAYER 2 LOSE',True, (255,0,0))
@@ -60,24 +82,10 @@ while game:
         player1.update_left()
         player2.update_right()
         ball.reset()
-
-        ball.rect.x += speed_x
-        ball.rect.y += speed_y
-
-        if ball.rect.y < 0 or ball.rect.y > 600:
-            speed_y *= -1
-
-        if sprite.collide_rect(ball,player1) or sprite.collide_rect(ball,player2):
-            speed_x *= -1
+        ball.update()
         
-        if ball.rect.x < 0:
-            finish = True
-            window.blit(lose1,(375,325))
-        if ball.rect.x > 900:
-            finish = True
-            window.blit(lose2,(375,325))
+       
         
 
     clock.tick(FPS)
     display.update()
-
